2daee870cf283d90963efc357f69ee5ea88cbb91
Jacob McFaul <mcjakedude@gmail.com>
2019-04-01 16:09:33 -0700

    Login function working

--- data/router/auth.js
+++ data/router/auth.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 const Auth = require('../helpers/authDB.js');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', restricted, (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 12)
     user.password = hash;
     
     Auth.add(user)
         .then(saved => {
+            req.session.user = saved
             res.status(201).json(saved)
         })
         .catch(error => {
@@ -24,7 +26,8 @@ router.post('/login', (req, res) => {
     Auth.findBy({ username })
         .first()
         .then(user => {
-            if (user) {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ message: `Welcome ${user.username}`})
             }else {
                 res.status(401).json({ message: 'Try Again'})
@@ -35,7 +38,15 @@ router.post('/login', (req, res) => {
         })
 })
 
-router.get('/user', (req,res) => {
+function restricted(req, res, next){
+    if(req.session && req.session.user){
+        next();
+    }else{
+        res.status(401).json({ message: 'You are not loggedin '})
+    }
+}
+
+router.get('/user',restricted, (req,res) => {
     Auth.find()
         .then(users => {
             res.json(users);
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const session = require('express-session');
 const auth = require('./data/router/auth.js');
+
+const sessionConfig = {
+    name: "jake",
+    secret: "keep it secret, keep it safe",
+    cookie: {
+        maxAge: 1000 * 60 * 15,
+        secure: false,
+    },
+    httpOnly: true,
+    resave:false,
+    saveUninitialized: false,
+}
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
-
+server.use(session(sessionConfig))
 server.use('/api/', auth);
 
 server.get('/', (req,res) => {
